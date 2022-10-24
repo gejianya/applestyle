0977cd40f726b3d6cb4317ff9d712576a62d02c0
gejianya <gjywin@163.com>
2022-10-24 18:20:10 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ module.exports = withPWA(
       return config
     },
     images: {
-      domains: ['static.anzifan.com', 'cdn.sspai.com', 'cdn.dribbble.com', 'image.freepik.com', 'avatars.githubusercontent.com', 'cdn.jsdelivr.net', 'image.cugxuan.cn', 'blog-static.mikuchan.top', 'amazonaws.com', 'img.zhheo.com', 'www.aohuiliu.fun', 'rxhsk.xicp.fun', 'www.fomal.cc', 'raw.githubusercontent.com', 'imgse.com'],
+      domains: ['avatars.githubusercontent.com', 'cdn.jsdelivr.net', 'img.zhheo.com', 'rxhsk.xicp.fun', 'raw.githubusercontent.com', 'imgse.com'],
     },
     pwa: {
       dest: "public",
